ff166c93c2b6bc563e8956e500466883d0b803bd
Milan Straka <milan@strakovi.com>
2017-05-11 21:56:02 +0200

    Add embedding_form_mincount.

--- src/trainer/trainer_morphodita_parsito.cpp
+++ src/trainer/trainer_morphodita_parsito.cpp
@@ -285,6 +285,7 @@ bool trainer_morphodita_parsito::train_parser(const vector<sentence>& training, 
       int embedding_feats = 20; if (!option_int(parser, "embedding_feats", embedding_feats, error)) return false;
       int embedding_xpostag = 0; if (!option_int(parser, "embedding_xpostag", embedding_xpostag, error)) return false;
       int embedding_form = 50; if (!option_int(parser, "embedding_form", embedding_form, error)) return false;
+      int embedding_form_mincount = 2; if (!option_int(parser, "embedding_form_mincount", embedding_form_mincount, error)) return false;
       int embedding_lemma = 0; if (!option_int(parser, "embedding_lemma", embedding_lemma, error)) return false;
       int embedding_deprel = 20; if (!option_int(parser, "embedding_deprel", embedding_deprel, error)) return false;
       string embeddings;
@@ -292,7 +293,7 @@ bool trainer_morphodita_parsito::train_parser(const vector<sentence>& training, 
       if (embedding_feats) embeddings.append("feats ").append(to_string(embedding_feats)).append(" 1\n");
       if (embedding_xpostag) embeddings.append("tag ").append(to_string(embedding_xpostag)).append(" 1\n");
       if (embedding_form) {
-        embeddings.append("form ").append(to_string(embedding_form)).append(" 2");
+        embeddings.append("form ").append(to_string(embedding_form)).append(" ").append(to_string(embedding_form_mincount));
         if (!option_str(parser, "embedding_form_file").empty()) embeddings.append(" ").append(option_str(parser, "embedding_form_file"));
         embeddings.push_back('\n');
       }
